7af6d6d45ec502646d8aad83690e571bca82743f
unknown <znbareej.1@gmail.com>
2022-10-10 22:27:38 -0700

    First commit

---
+++ src/Components/Helpers/Wrapper.js
@@ -0,0 +1,8 @@
+function Wrapper(props) {
+  return props.children;
+  
+  //----children prop holds all the content you pass b/w the
+  //    opening and closing tag of your custom component.
+};
+
+export default Wrapper;
